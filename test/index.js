61257d9f094b00fdccf9623bce95144655c05a15
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-19 11:16:50 +0200

    Test with new

--- test/index.js
+++ test/index.js
@@ -1,11 +1,14 @@
 const test = require('tape');
 const Otto = require('../');
 
-test('will default', (t) => {
+test('will fake new and default', (t) => {
   const otto = Otto();
   const grid = otto();
+  const mock = new Otto()();
 
-  t.ok(grid.length);
+  t.ok(grid.length, `grid size is ${grid.length} `);
+  t.ok(mock.length, `mock size is ${mock.length} `);
+  t.equals(grid.length, mock.length, `grid size === mock size`);
   t.end();
 });
 
