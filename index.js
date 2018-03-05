fe1eed75616fdd631a7d3f0ef0fa9b0985cb2b9b
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-03-05 06:22:47 +0100

    module: buble not the cjs version

--- index.js
+++ index.js
@@ -3,20 +3,19 @@
 var otto = require('@thewhodidthis/otto');
 
 // # Life
-// Just another game of life runner
 
-var mySum = function (a, b) { return a + b; };
-var mooreEnds = function (n) { return [-1, 1, -n, n, -1 - n, 1 - n, -1 + n, 1 + n]; };
+const mySum = (a, b) => a + b;
+const mooreEnds = n => [-1, 1, -n, n, -1 - n, 1 - n, -1 + n, 1 + n];
 
-var life = function (from) {
-  var size = (from && from.size) || 1;
-  var grid = { size: size * size };
+const life = (from) => {
+  const size = (from && from.size) || 1;
+  const grid = { size: size * size };
 
-  var data = Object.assign({
+  const data = Object.assign({
     ends: mooreEnds(size),
-    seed: function () { return Math.floor(Math.random() * 2) % 2; },
-    stat: function (hood, code, flag) {
-      var stats = hood.reduce(mySum);
+    seed: () => Math.floor(Math.random() * 2) % 2,
+    stat: (hood, code, flag) => {
+      const stats = hood.reduce(mySum);
 
       if (flag && (stats <= 1 || stats >= 4)) {
         return 0
@@ -34,4 +33,3 @@ var life = function (from) {
 };
 
 module.exports = life;
-
